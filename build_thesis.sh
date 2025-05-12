1a8ace83fbcbc0c44252de7d4364b35747c37474
airzocker <fbecker@software-manufaktur.de>
2025-05-12 20:44:43 +0200

    Removed thesis files from this repo

--- build_thesis.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd ./thesis/
-if [ "$1" = "open" ]; then
-  pdflatex -output-directory=./out/ ./Bachelor_english.tex && okular ./out/Bachelor_english.pdf
-else
-  pdflatex -output-directory=./out/ ./Bachelor_english.tex
-fi
